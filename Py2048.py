a4293f12de64f6c8fec29fe773907f19650a850d
FrozenHydrant <40309655+FrozenHydrant@users.noreply.github.com>
2018-06-16 18:00:40 -0700

    Fixed score, few minor changes

--- Py2048.py
+++ Py2048.py
@@ -12,6 +12,7 @@ mainbox.withdraw()
 
 pygame.init()
 gamefont = pygame.font.SysFont('Consolas', 30)
+pygame.display.set_caption('2048')
 
 BOXWIDTH = 1037
 BOXHEIGHT = 537
@@ -34,6 +35,7 @@ tile_16 = pygame.image.load(os.getcwd() + '\\py2048_assets\\tiles\\2048_16.jpg')
 tile_32 = pygame.image.load(os.getcwd() + '\\py2048_assets\\tiles\\2048_32.jpg')
 tile_64 = pygame.image.load(os.getcwd() + '\\py2048_assets\\tiles\\2048_64.jpg')
 tile_128 = pygame.image.load(os.getcwd() + '\\py2048_assets\\tiles\\2048_128.jpg')
+tile_256 = pygame.image.load(os.getcwd() + '\\py2048_assets\\tiles\\2048_256.jpg')
 tile_blank = pygame.image.load(os.getcwd() + '\\py2048_assets\\tiles\\2048_blank.jpg')
 
 playermoved = False
@@ -80,8 +82,7 @@ def random_tile():
     if not templist1:
         gameover()
         game_end = True
-    for lol in range(random.randint(1, 4)):
-        tiles[random.choice(templist1)] = [random.choice(random_tiles)]
+    tiles[random.choice(templist1)] = [random.choice(random_tiles)]
     # print(tiles)
 
 
@@ -107,7 +108,7 @@ def move_tile(direction):
                         tiles[item] = []
                     elif tiles[item + 1] == tiles[item]:
                         tiles[item + 1] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
                 if iteration == 1 and tiles[item] and item == 1 or item == 5 or item == 9 or item == 13:
                     if not tiles[item + 2] and not tiles[item + 1]:
@@ -115,14 +116,14 @@ def move_tile(direction):
                         tiles[item] = []
                     elif tiles[item + 2] == tiles[item] and not tiles[item + 1]:
                         tiles[item + 2] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
                     elif not tiles[item + 1]:
                         tiles[item + 1] = tiles[item]
                         tiles[item] = []
                     elif tiles[item + 1] == tiles[item]:
                         tiles[item + 1] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
                 if iteration == 2 and tiles[item] and item == 0 or item == 4 or item == 8 or item == 12:
                     if not tiles[item + 3] and not tiles[item + 2] and not tiles[item + 1]:
@@ -130,21 +131,21 @@ def move_tile(direction):
                         tiles[item] = []
                     elif tiles[item + 3] == tiles[item] and not tiles[item + 2] and not tiles[item + 1]:
                         tiles[item + 3] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
                     elif not tiles[item + 2] and not tiles[item + 1]:
                         tiles[item + 2] = tiles[item]
                         tiles[item] = []
                     elif tiles[item + 2] == tiles[item] and not tiles[item + 1]:
                         tiles[item + 2] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
                     elif not tiles[item + 1]:
                         tiles[item + 1] = tiles[item]
                         tiles[item] = []
                     elif tiles[item + 1] == tiles[item]:
                         tiles[item + 1] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
     elif direction == 'up':
         for iteration in range(3):
@@ -155,7 +156,7 @@ def move_tile(direction):
                         tiles[item] = []
                     elif tiles[item - 4] == tiles[item]:
                         tiles[item - 4] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
                 if iteration == 1 and item == 8 or item == 9 or item == 10 or item == 11:
                     if not tiles[item - 8] and not tiles[item - 4]:
@@ -163,14 +164,14 @@ def move_tile(direction):
                         tiles[item] = []
                     elif tiles[item - 8] == tiles[item] and not tiles[item - 4]:
                         tiles[item - 8] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
                     elif not tiles[item - 4]:
                         tiles[item - 4] = tiles[item]
                         tiles[item] = []
                     elif tiles[item - 4] == tiles[item]:
                         tiles[item - 4] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
                 if iteration == 2 and item == 12 or item == 13 or item == 14 or item == 15:
                     if not tiles[item - 12] and not tiles[item - 8] and not tiles[item - 4]:
@@ -178,21 +179,21 @@ def move_tile(direction):
                         tiles[item] = []
                     elif tiles[item - 12] == tiles[item] and not tiles[item - 8] and not tiles[item - 4]:
                         tiles[item - 12] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
                     elif not tiles[item - 8] and not tiles[item - 4]:
                         tiles[item - 8] = tiles[item]
                         tiles[item] = []
                     elif tiles[item - 8] == tiles[item] and not tiles[item - 4]:
                         tiles[item - 8] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
                     elif not tiles[item - 4]:
                         tiles[item - 4] = tiles[item]
                         tiles[item] = []
                     elif tiles[item - 4] == tiles[item]:
                         tiles[item - 4] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
     elif direction == 'left':
         for iteration in range(3):
@@ -203,7 +204,7 @@ def move_tile(direction):
                         tiles[item] = []
                     elif tiles[item - 1] == tiles[item]:
                         tiles[item - 1] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
                 if iteration == 1 and tiles[item] and item == 2 or item == 6 or item == 10 or item == 14:
                     if not tiles[item - 2] and not tiles[item - 1]:
@@ -211,14 +212,14 @@ def move_tile(direction):
                         tiles[item] = []
                     elif tiles[item - 2] == tiles[item] and not tiles[item - 1]:
                         tiles[item - 2] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
                     elif not tiles[item - 1]:
                         tiles[item - 1] = tiles[item]
                         tiles[item] = []
                     elif tiles[item - 1] == tiles[item]:
                         tiles[item - 1] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
                 if iteration == 2 and tiles[item] and item == 3 or item == 7 or item == 11 or item == 15:
                     if not tiles[item - 3] and not tiles[item - 2] and not tiles[item - 1]:
@@ -226,21 +227,21 @@ def move_tile(direction):
                         tiles[item] = []
                     elif tiles[item - 3] == tiles[item] and not tiles[item - 2] and not tiles[item - 1]:
                         tiles[item - 3] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
                     elif not tiles[item - 2] and not tiles[item - 1]:
                         tiles[item - 2] = tiles[item]
                         tiles[item] = []
                     elif tiles[item - 2] == tiles[item] and not tiles[item - 1]:
                         tiles[item - 2] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
                     elif not tiles[item - 1]:
                         tiles[item - 1] = tiles[item]
                         tiles[item] = []
                     elif tiles[item - 1] == tiles[item]:
                         tiles[item - 1] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
     elif direction == 'down':
         for iteration in range(3):
@@ -251,7 +252,7 @@ def move_tile(direction):
                         tiles[item] = []
                     elif tiles[item + 4] == tiles[item]:
                         tiles[item + 4] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
                 if iteration == 1 and item == 4 or item == 5 or item == 6 or item == 7:
                     if not tiles[item + 8] and not tiles[item + 4]:
@@ -259,14 +260,14 @@ def move_tile(direction):
                         tiles[item] = []
                     elif tiles[item + 8] == tiles[item] and not tiles[item + 4]:
                         tiles[item + 8] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
                     elif not tiles[item + 4]:
                         tiles[item + 4] = tiles[item]
                         tiles[item] = []
                     elif tiles[item + 4] == tiles[item]:
                         tiles[item + 4] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
                 if iteration == 2 and item == 0 or item == 1 or item == 2 or item == 3:
                     if not tiles[item + 12] and not tiles[item + 8] and not tiles[item + 4]:
@@ -274,21 +275,21 @@ def move_tile(direction):
                         tiles[item] = []
                     elif tiles[item + 12] == tiles[item] and not tiles[item + 8] and not tiles[item + 4]:
                         tiles[item + 12] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
                     elif not tiles[item + 8] and not tiles[item + 4]:
                         tiles[item + 8] = tiles[item]
                         tiles[item] = []
                     elif tiles[item + 8] == tiles[item] and not tiles[item + 4]:
                         tiles[item + 8] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
                     elif not tiles[item + 4]:
                         tiles[item + 4] = tiles[item]
                         tiles[item] = []
                     elif tiles[item + 4] == tiles[item]:
                         tiles[item + 4] = [str(tiles[item][0]).split('_')[0] + '_' + str(int(str(tiles[item][0]).split('_')[1]) * 2)]
-                        score += int(str(tiles[item][0]).split('_')[1])
+                        score += int(str(tiles[item][0]).split('_')[1]) * 2
                         tiles[item] = []
     playermoved = True
 
@@ -300,6 +301,7 @@ def gameover():
     pygame.display.update()
     time.sleep(2)
     sys.exit()
+
 
 if __name__ == '__main__':
     main()
